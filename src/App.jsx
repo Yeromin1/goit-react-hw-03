abce0e3282823081f597895671b0949ec02eb1dc
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2024-12-12 17:21:52 +0200

    Contact-fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
 function App() {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState("");
+  const initialContacts = [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ];
 
-  useEffect(() => {
-    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
-    if (savedContacts) {
-      setContacts(savedContacts);
-    }
-  }, []);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+  });
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     if (contacts.length > 0) {
@@ -29,6 +32,7 @@ function App() {
     setFilter(e.target.value);
   };
 
+  // Добавление контакта
   const addContact = (name, number) => {
     const newContact = {
       id: nanoid(),
